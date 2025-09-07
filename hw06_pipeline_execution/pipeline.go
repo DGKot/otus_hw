9a9b6f077b817ce0e96f69ffc1640f5c2b86eeaf
Дмитрий Котляр <kotlyar_dg@MacBook-Pro-Dmitrij.local>
2025-09-08 00:10:33 +0300

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -10,5 +10,39 @@ type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	// Place your code here.
-	return nil
+	in = cancelStage(in, done)
+	for _, stage := range stages {
+		in = cancelStage(stage(in), done)
+	}
+
+	return in
+}
+
+func cancelStage(in In, done In) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				for v := range in {
+					_ = v
+				}
+				return
+			case val, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case <-done:
+					for v := range in {
+						_ = v
+					}
+					return
+				case out <- val:
+				}
+			}
+		}
+	}()
+	return out
 }
