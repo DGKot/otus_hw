bdd6ef6ded9ebecb76430c8016e7e24b5bafb232
Дмитрий Котляр <kotlyar_dg@MacBook-Pro-Dmitrij.local>
2025-09-11 22:13:35 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
 )
 
 var (
@@ -9,7 +12,55 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
-func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+type PbWritter struct {
+	current int64
+	total   int64
+}
+
+func (pb *PbWritter) Write(p []byte) (n int, err error) {
+	n = len(p)
+	pb.current += int64(n)
+	fmt.Printf("Progress of copying file: %.2f%%\n", float64(pb.current)*100/float64(pb.total))
+	return n, nil
+}
+
+func CopyCore(to io.Writer, from io.Reader, limit int64) error {
+	progresWritter := &PbWritter{total: limit}
+	_, err := io.CopyN(io.MultiWriter(to, progresWritter), from, limit)
+	if err != nil {
+		return err
+	}
 	return nil
+}
+
+func Copy(fromPath, toPath string, offset, limit int64) error {
+	file, err := os.Open(fromPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer file.Close()
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	if fileInfo.Size() < offset {
+		return ErrOffsetExceedsFileSize
+	}
+	resFile, err := os.OpenFile(toPath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0o600)
+	if err != nil {
+		return err
+	}
+	defer resFile.Close()
+
+	_, err = file.Seek(offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+	if limit == 0 {
+		limit = fileInfo.Size()
+	} else {
+		limit = min(limit, fileInfo.Size()-offset)
+	}
+
+	return CopyCore(resFile, file, limit)
 }
