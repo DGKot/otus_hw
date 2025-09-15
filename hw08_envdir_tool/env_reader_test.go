4d0e0986e3a1c9612f0f34f2f8ecb7b45e1a5904
Дмитрий Котляр <kotlyar_dg@MacBook-Pro-Dmitrij.local>
2025-09-15 21:46:49 +0300

    HW8 is completed

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,31 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
 	// Place your code here
+	expected := make(Environment)
+	expected["BAR"] = EnvValue{Value: "bar"}
+	expected["EMPTY"] = EnvValue{Value: ""}
+	expected["FOO"] = EnvValue{Value: "   foo\nwith new line"}
+	expected["HELLO"] = EnvValue{Value: "\"hello\""}
+	expected["UNSET"] = EnvValue{Value: "", NeedRemove: true}
+
+	t.Run("Read dir standart", func(t *testing.T) {
+		result, err := ReadDir("./testdata/env")
+		if err != nil {
+			t.Fatal(err)
+		}
+		require.Equal(t, expected, result)
+	})
+
+	t.Run("Environment to string", func(t *testing.T) {
+		expectedString := []string{"BAR=bar", "EMPTY=", "FOO=   foo\nwith new line", "HELLO=\"hello\"", "UNSET="}
+		res := expected.ToSliceString()
+		require.Equal(t, expectedString, res)
+	})
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"os"
+)
+
 func main() {
 	// Place your code here.
+	path := os.Args[1]
+	env, err := ReadDir(path)
+	if err != nil {
+		return
+	}
+	code := RunCmd(os.Args[2:], env)
+	os.Exit(code)
 }
