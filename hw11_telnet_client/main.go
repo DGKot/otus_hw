700bfa408af9284b007a253d446a634a6202eb6d
Дмитрий Котляр <kotlyar_dg@MacBook-Pro-Dmitrij.local>
2025-10-04 15:18:25 +0300

    HW10 is completed

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,50 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"net"
+	"os"
+	"sync"
+	"time"
+)
+
 func main() {
 	// Place your code here,
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	var timeout time.Duration
+	flag.DurationVar(&timeout, "timeout", time.Second*10, "timeout connection")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 2 {
+		log.Fatal("Miss port for connect")
+	}
+	host := args[0]
+	port := args[1]
+
+	client := NewTelnetClient(net.JoinHostPort(host, port), timeout, os.Stdin, os.Stdout)
+	err := client.Connect()
+	if err != nil {
+		log.Fatal("error connect to server:", err)
+	}
+
+	var closeOnce sync.Once
+	closeCh := make(chan struct{})
+
+	go func() {
+		client.Send()
+		closeOnce.Do(func() {
+			client.Close()
+			close(closeCh)
+		})
+	}()
+
+	go func() {
+		client.Receive()
+		closeOnce.Do(func() {
+			client.Close()
+			close(closeCh)
+		})
+	}()
+	<-closeCh
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,9 +14,54 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type telnetClient struct {
+	conn    net.Conn
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+}
+
+var ErrNotConnect = errors.New("client didn't connect")
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
 	// Place your code here.
-	return nil
+	return &telnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (t *telnetClient) Connect() error {
+	var err error
+	t.conn, err = net.DialTimeout("tcp", t.address, t.timeout)
+	return err
+}
+
+func (t *telnetClient) Send() error {
+	if t.conn == nil {
+		return ErrNotConnect
+	}
+	_, err := io.Copy(t.conn, t.in)
+	return err
+}
+
+func (t *telnetClient) Receive() error {
+	if t.conn == nil {
+		return ErrNotConnect
+	}
+	_, err := io.Copy(t.out, t.conn)
+	return err
+}
+
+func (t *telnetClient) Close() error {
+	if t.conn == nil {
+		return nil
+	}
+	t.out.Write([]byte("Connect closed"))
+	return t.conn.Close()
 }
 
 // Place your code here.
